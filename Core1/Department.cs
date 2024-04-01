eaf4ca3f7de60445dc5d9384556eb3e901c8abf7
GulgazSuleymanli <bd7tk5y26@code.edu.az>
2024-04-01 19:42:29 +0400

    try error

---
+++ Core1/Department.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core1
+{
+     public class Department
+    {
+        public int No { set; get; }
+        public int EmployeeLimit
+        {
+            get
+            {
+                return EmployeeLimit;
+            }
+            set
+            {
+                if(EmployeeLimit>0 && EmployeeLimit <20)
+                {
+                    EmployeeLimit = value;
+                }
+            }
+        }
+    }
+}
---
+++ Core1/Employee.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core1
+{
+    public class Employee
+    {
+        public int IdForObject { get; set; }
+        private static int _idForClass { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public byte _age;
+        public byte Age { 
+            get { return _age; }
+            set
+            {
+                if(value > 0)
+                {
+                    _age = value;
+                }
+            }
+        }
+
+        public int DepartmentNo { get; set; }
+        public string Position { get; set; }
+
+        private double _salary;
+        public double Salary { 
+            get { return _salary; }
+            set
+            {
+                if(value > 0)
+                {
+                    _salary = value;
+                }
+            }
+        }
+
+        public Employee()
+        {
+            _idForClass++;
+            IdForObject = _idForClass;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Name: {Name}, Surname: {Surname}, Age: {Age}, DepartmentNo: {DepartmentNo}, Position: {Position}, Salary: {Salary}");
+        }
+
+    }
+}
